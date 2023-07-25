96f64e229c544112640c19b962c986a7f6542e2e
HATJ66620201 <hatj66620201@Jinane.myguest.virtualbox.org>
2023-07-25 05:00:20 -0400

    J ai ajoute validation flask mais ca ne se redirige pas vers erreur.html

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, url_for
 from database import Database
+import random
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
@@ -19,26 +20,101 @@ def close_connection(exception):
 def form():
     return render_template('adoption.html')
 
-@app.route('/<id_animal>')
+
+@app.route('/animal/<id_animal>')
 def animal_page(id_animal):
     animal = get_db().get_animal(id_animal)
-    if animal is None:
-        return redirect(404)
-    return render_template('resultat_recherche.html', animal=animal)
+    if animal:return render_template('resultat_recherche.html', animal=animal)
+    elif animal is None: return redirect(404)
+
+
+
+
+
+
 
 @app.route("/soumettre", methods=["POST"])
 def soumettre():
-    if len(request.form["nom"]) > 25 or \
-    len(request.form["description"]) > 500 or \
-    len(request.form["adresse"]) > 75 or \
-    len(request.form["ville"]) > 75 or \
-    len(request.form["codepostal"]) > 7 or \
-    len(request.form["email"]) > 80:
-        return redirect('/erreur')
+    
+    if len(request.form["nom"]) < 3 or len(request.form["nom"])>20:
+      return redirect("/erreur")
+    
+    elif int(request.form["age"]) <0 or int(request.form["age"])>30:
+       return redirect("/erreur")
 
+
+    elif len(request.form["description"]) > 500: 
+        return redirect("/erreur")
+    
+    elif len(request.form["adresse"]) > 75:
+      return redirect("/erreur")
+    
+    elif len(request.form["ville"]) > 75:
+      return redirect("/erreur")
+    
+    elif len(request.form["codepostal"]) > 7:
+       return redirect("/erreur")
+    
+    elif len(request.form["email"]) > 80:
+       return redirect("/erreur")
+    
+
+
+    elif not request.form["nom"].strip() or not request.form["nom"]:
+            return redirect("/erreur")
+    
+    elif not request.form["description"].strip() or not request.form["nom"]:
+          return redirect("/erreur")
+    
+    elif not request.form["adresse"].strip() or not request.form["nom"]:
+            return redirect("/erreur")
+    
+    elif not request.form["ville"].strip() or not request.form["nom"]:
+          return redirect("/erreur")
+    
+    elif not request.form["codepostal"].strip() or not request.form["nom"]:
+            return redirect("/erreur")
+    
+    elif not request.form["email"].strip() or not request.form["nom"]:
+          return redirect("/erreur")
+    
+    elif not request.form["age"].strip() or not request.form["age"]:
+       return redirect("/erreur")
+    
+    
+
+    elif "," in request.form["nom"]:
+      return redirect("/erreur")   
+             
+    elif "," in  request.form["description"]:
+      return redirect("/erreur")
+    
+    elif "," in request.form["adresse"]:
+        return redirect("/erreur")
+    
+    elif "," in request.form["ville"]:
+        return redirect("/erreur")
+    
+    elif "," in request.form["codepostal"]:
+        return redirect("/erreur")
+            
+    elif "," in request.form["email"]:
+        return redirect("/erreur")
+          
+    elif "," in request.form["age"]:
+        return redirect("/erreur")
+
+    
+     
 @app.route("/erreur")
 def erreur():
-    return render_template('erreur.html')  # assuming you have an 'erreur.html' template
+    return render_template('erreur.html') 
+
+@app.route('/accueil')
+def accueil():
+    
+    liste=random.choices(get_db().get_animaux(), k=5)
+    return render_template('accueil.html', liste=liste)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -46,3 +122,5 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
