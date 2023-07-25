6273abe17353590699b9dac442a3aebb036e8bc1
BrainChemist2005 <zackariaebouargan@gmail.com>
2023-07-25 03:30:26 -0400

    J'ai ajoute la page dynamique qui affiche des nouveaux animaux aleatoirement a chaque fois

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, g
 from database import Database
+import random
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
@@ -15,16 +16,30 @@ def close_connection(exception):
     if db is not None:
         db.disconnect()
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def form():
-    return render_template('adoption.html')
+    animaux = get_db().get_animaux()
+    if len(animaux) >= 5:
+        animauxHasard = random.sample(animaux, 5)
+    else:
+        animauxHasard = animaux
 
-@app.route('/animal/<id_animal>')
+    return render_template('accueil.html', animaux=animauxHasard)
+
+@app.route('/animal/<int:id_animal>', methods=['GET'])
 def animal_page(id_animal):
     animal = get_db().get_animal(id_animal)
     if animal is None:
         return redirect(404)
     return render_template('resultat_recherche.html', animal=animal)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/adoption")
+def adoption():
+    return render_template('adoption.html')
 
 @app.route("/soumettre", methods=["POST"])
 def soumettre():
@@ -35,11 +50,14 @@ def soumettre():
     len(request.form["codepostal"]) > 7 or \
     len(request.form["email"]) > 80:
         return redirect('/erreur')
+    
+    
+    get_db().add_animal(request.form["nom"], request.form["espece"], request.form["race"], 
+                        request.form["age"], request.form["description"], request.form["email"],
+                        request.form["adresse"], request.form["ville"], request.form["codepostal"])
+    
+    
+    return redirect('/succes')
 
-
-@app.errorhandler(404)
-def not_found(e):
-    return render_template('404.html'), 404
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
